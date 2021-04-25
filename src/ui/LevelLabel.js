b618136cfe98f651e8b02a1ac2d60310dcc2a76d
michelangelo giacomelli <michelangelog@gmail.com>
2021-04-25 17:56:49 +0200

    indent

--- src/ui/LevelLabel.js
+++ src/ui/LevelLabel.js
@@ -4,25 +4,25 @@ const format_level = (level) => `Level: ${level}`;
 
 export default class LevelLabel extends Phaser.GameObjects.Text
 {
-	constructor(scene, x, y, level, style)
-	{
-		super(scene, x, y, format_level(level), style);
-		this.level = level;
-	}
+    constructor(scene, x, y, level, style)
+    {
+        super(scene, x, y, format_level(level), style);
+        this.level = level;
+    }
 
-	setScore(level)
-	{
-		this.level  = level
-		this.updateScoreText();
-	}
+    setScore(level)
+    {
+        this.level  = level
+        this.updateScoreText();
+    }
 
-	add(level)
-	{
-		this.setScore(this.level + level);
-	}
+    add(level)
+    {
+        this.setScore(this.level + level);
+    }
 
-	updateScoreText()
-	{
-		this.setText(format_level(this.level));
-	}
+    updateScoreText()
+    {
+        this.setText(format_level(this.level));
+    }
 }
--- src/ui/ScoreLabel.js
+++ src/ui/ScoreLabel.js
@@ -4,25 +4,25 @@ const formatScore = (score) => `Score: ${score}`;
 
 export default class ScoreLabel extends Phaser.GameObjects.Text
 {
-	constructor(scene, x, y, score, style)
-	{
-		super(scene, x, y, formatScore(score), style);
-		this.score = score;
-	}
+    constructor(scene, x, y, score, style)
+    {
+        super(scene, x, y, formatScore(score), style);
+        this.score = score;
+    }
 
-	setScore(score)
-	{
-		this.score  = score
-		this.updateScoreText();
-	}
+    setScore(score)
+    {
+        this.score  = score
+        this.updateScoreText();
+    }
 
-	add(points)
-	{
-		this.setScore(this.score + points);
-	}
+    add(points)
+    {
+        this.setScore(this.score + points);
+    }
 
-	updateScoreText()
-	{
-		this.setText(formatScore(this.score));
-	}
+    updateScoreText()
+    {
+        this.setText(formatScore(this.score));
+    }
 }
